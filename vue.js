6f2f73b538d7c3e30b2d10fe0b025879d4d87eba
Sergey <SergeySm128@gmail.com>
2021-01-04 19:10:59 +0200

    done hw1

--- vue.js
+++ vue.js
@@ -1,11 +1,8 @@
-// вы можете как угодно изменять программу и код
-// добавлять любые переменные и модели
-// ваша задача реализовать так, как показано на видео, чтобы оно работало
 
 const App = {
   data() {
     return {
-      activeIndex: 0, // то, что позволяет определить текущий активный шаг
+      activeIndex: 0,
       steps: [
         {title: 'Основы', text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'},
         {title: 'Компоненты', text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'},
@@ -16,24 +13,20 @@ const App = {
     }
   },
   methods: {
-    prev() {
-      // когда нажимаем кнопку назад
-    },
-    reset() {
-      // начать заново
-    },
-    nextOfFinish() {
-      // кнопка вперед или закончить
-    },
-    setActive(idx) {
-      // когда нажимаем на определенный шаг
-    }
+    prev() {this.activeIndex--},
+    reset() {this.activeIndex = 0},
+    nextOfFinish() {this.activeIndex++},
+    setActive(idx) {this.activeIndex = idx}
   },
   computed: {
-    // тут стоит определить несколько свойств:
-    // 1. текущий выбранный шаг
-    // 2. выключена ли кнопка назад
-    // 3. находимся ли мы на последнем шаге
+    getActiveText() {
+      return this.steps[this.activeIndex] ?
+          this.steps[this.activeIndex].text :
+          this.steps[this.steps.length - 1].text
+    },
+    isDisabledPrevButton() {return this.activeIndex === 0},
+    isLastStep() {return this.activeIndex + 1 === this.steps.length},
+    isFinish() {return this.activeIndex === this.steps.length}
   }
 }
 
